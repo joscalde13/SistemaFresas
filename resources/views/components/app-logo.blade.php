9e3767df92e65e417723180d0bd753f6df12f859
Jostin <jostin@example.com>
2025-09-13 17:23:52 -0600

    logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,5 +2,5 @@
     <img src="{{ asset('images/fresitas.jpeg') }}" alt="Logo de Fresas" class="h-8 w-8 object-contain" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Las Fresitas</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">Chica Fresita</span>
 </div>
